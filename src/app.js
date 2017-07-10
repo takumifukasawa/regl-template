f8e64a82b050609cfc41244c1b0a418409e20103
takumifukasawa <fukapon912@gmail.com>
2017-07-10 09:47:12 +0900

    create template

--- src/app.js
+++ src/app.js
@@ -1,7 +1,60 @@
 
-const name = 'javascript-starter';
+import reglwebgl from "regl";
 
-Promise.resolve()
-  .then(() => {
-    console.log(name);
+const regl = reglwebgl();
+
+const frag = `
+precision mediump float;
+uniform vec4 color;
+
+void main(void) {
+  gl_FragColor = color;
+}
+`;
+
+const vert = `
+precision mediump float;
+attribute vec2 position;
+
+void main(void) {
+  gl_Position = vec4(position, 0, 1.);
+}
+`;
+
+const drawTriangle = regl({
+  frag,
+  vert,
+  attributes: {
+    position: [
+      [-1, 1],
+      [-1, -1],
+      [1, 1],
+      [1, -1],
+      [1, 1],
+      [-1, -1]
+    ]
+  },
+  uniforms: {
+    color: regl.prop("color")
+  },
+  count: 6
+});
+
+regl.frame(({time}) => {
+  regl.clear({
+    color: [0, 0, 0, 1],
+    depth: 1
   });
+
+  drawTriangle({
+    color: [
+      Math.sin(time * .1),
+      Math.cos(time * .2),
+      Math.sin(time * .3),
+      1
+    ]
+  });
+});
+
+
+
